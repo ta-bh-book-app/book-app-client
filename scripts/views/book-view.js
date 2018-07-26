033d04acd96e900f134cb0c421e0e68f547c912f
tganyan <tganyan@gmail.com>
2018-07-26 12:46:18 -0700

    adding book id to path in handelbars template, linking routes script to html

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -12,6 +12,22 @@ var app = app || {};
     });
   }
 
+  // need to pass the context object
+  // context needed in route
+  // route is going to need context as well
+  // passing in ctx
+  // empty
+  // show only
+  // append
+  bookView.initDetailView = () => {
+    // app.Book.fetchOne()
+    app.showOnly('#detail-view');
+  }
+
+  bookView.initFormView = () => {
+    app.showOnly('#list-view');
+  }
+
   module.bookView = bookView;
 })(app);
 
