f138cdf182843ab8f33ece23f4c67150e3dea042
tganyan <tganyan@gmail.com>
2018-07-28 15:27:59 -0700

    troubleshooting page routes

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -1,9 +1,9 @@
-// List view books by title and author with rendered image
-// The below code runs the app.bookview.initIndexPage function when returning to the root page. The app.Book.fetchAll is also run on initial page load; this call can be found at the bottom of book-view.js 
-page('/', context => app.Book.fetchAll(app.bookView.initIndexPage));
+page('/'
+  , (context, next) => app.Book.fetchAll(() => app.bookView.initIndexPage(context, next))
+);
+page('/books/new'
+  , context => app.bookView.initFormView(context)
+);
 page('/books/:book_id', context => app.Book.fetchOne(context, app.bookView.initDetailView));
-// use context fetchOne using context as an arguement, plus the init call as the second arguement
-// look at rdeirect pass context objects
-// 
-page('/books/new', app.bookView.initFormView);
+
 page();
